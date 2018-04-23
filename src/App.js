430c73758fcb9d4440a23d7cc50b4b88684a2910
Hwan Seob Lee <bestseob93@gmail.com>
2018-04-23 17:22:49 +0900

    current players, competing ether added

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import web3 from './web3';
 import lotto from './lotto';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      manager: ''
-    };
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
   }
 
   async componentDidMount() {
     try {
       const manager = await lotto.methods.manager().call();
+      const players = await lotto.methods.getPlayer().call();
+      const balance = await web3.eth.getBalance(lotto.options.address);
+
       this.setState({
-        manager
+        manager,
+        players,
+        balance
       });
 
     } catch (e) {
@@ -31,6 +34,8 @@ class App extends Component {
         <h2>Lotto Contract</h2>
         <p>this contract is managed by</p>
         <p>{this.state.manager}</p>
+        <p>there are currently {this.state.players.length} people</p>
+        <p>competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
       </div>
     );
   }
